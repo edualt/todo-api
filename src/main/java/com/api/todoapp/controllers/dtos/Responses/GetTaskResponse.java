3b4b1972ac30bc679c363a241e5b7ace2cd3e678
edualt <eduardosebastianhdz503@gmail.com>
2022-10-04 18:53:38 -0500

    feat(responses): add create task response

---
+++ src/main/java/com/api/todoapp/controllers/dtos/Responses/GetTaskResponse.java
@@ -0,0 +1,20 @@
+package com.api.todoapp.controllers.dtos.Responses;
+
+import com.api.todoapp.constants.TaskStatusEnum;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter @Setter
+public class GetTaskResponse {
+
+    private Long id;
+    private String title;
+    private String description;
+    private LocalDateTime createdDate;
+    private LocalDateTime todoDate;
+    private Boolean finished;
+    private TaskStatusEnum taskStatus;
+
+}
