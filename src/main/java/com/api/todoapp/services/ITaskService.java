9094f3f4d405b4f12274a97f8ef5dd4813767fbd
edualt <eduardosebastianhdz503@gmail.com>
2022-10-04 18:57:04 -0500

    add new methods

--- src/main/java/com/api/todoapp/services/ITaskService.java
+++ src/main/java/com/api/todoapp/services/ITaskService.java
@@ -1,10 +1,21 @@
 package com.api.todoapp.services;
 
 import com.api.todoapp.controllers.dtos.Requests.CreateTaskRequest;
+import com.api.todoapp.controllers.dtos.Requests.UpdateTaskRequest;
 import com.api.todoapp.controllers.dtos.Responses.CreateTaskResponse;
+import com.api.todoapp.controllers.dtos.Responses.GetTaskResponse;
+import com.api.todoapp.controllers.dtos.Responses.UpdateTaskResponse;
+
+import java.util.List;
 
 public interface ITaskService {
 
+    List<GetTaskResponse> list();
+
+    GetTaskResponse get(Long id);
+
     CreateTaskResponse create(CreateTaskRequest request);
+
+    UpdateTaskResponse update(Long id, UpdateTaskRequest request);
 
 }
