04f70dfa12eb1fb83c7c8d0f8830a938b83ca65c
edualt <eduardosebastianhdz503@gmail.com>
2022-10-04 18:52:59 -0500

    modified anotations and rename variables

--- src/main/java/com/api/todoapp/controllers/dtos/Responses/CreateTaskResponse.java
+++ src/main/java/com/api/todoapp/controllers/dtos/Responses/CreateTaskResponse.java
@@ -1,18 +1,20 @@
 package com.api.todoapp.controllers.dtos.Responses;
 
 import com.api.todoapp.constants.TaskStatusEnum;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-@Data
+@Getter @Setter
 public class CreateTaskResponse {
 
+    private Long id;
     private String title;
     private String description;
     private LocalDateTime createdDate;
-    private LocalDateTime eta;
+    private LocalDateTime todoDate;
     private Boolean finished;
-    private TaskStatusEnum taskStatusEnum;
+    private TaskStatusEnum taskStatus;
 
 }
