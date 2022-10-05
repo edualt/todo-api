660dededc3566bd92b5a021b4567466b59af0848
edualt <eduardosebastianhdz503@gmail.com>
2022-10-04 19:03:51 -0500

    add new allowed methods

--- src/main/java/com/api/todoapp/controllers/TaskController.java
+++ src/main/java/com/api/todoapp/controllers/TaskController.java
@@ -1,10 +1,15 @@
 package com.api.todoapp.controllers;
 
 import com.api.todoapp.controllers.dtos.Requests.CreateTaskRequest;
+import com.api.todoapp.controllers.dtos.Requests.UpdateTaskRequest;
 import com.api.todoapp.controllers.dtos.Responses.CreateTaskResponse;
+import com.api.todoapp.controllers.dtos.Responses.GetTaskResponse;
+import com.api.todoapp.controllers.dtos.Responses.UpdateTaskResponse;
 import com.api.todoapp.services.ITaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("tasks")
@@ -13,9 +18,24 @@ public class TaskController {
     @Autowired
     private ITaskService service;
 
+    @GetMapping
+    public List<GetTaskResponse> list(){
+        return service.list();
+    }
+
+    @GetMapping("{id}")
+    public GetTaskResponse get(@PathVariable Long id){
+        return service.get(id);
+    }
+
     @PostMapping
     public CreateTaskResponse create(@RequestBody CreateTaskRequest request){
         return service.create(request);
+    }
+
+    @PutMapping("{id}")
+    public UpdateTaskResponse update(@PathVariable Long id, @RequestBody UpdateTaskRequest request){
+        return service.update(id, request);
     }
 
 }
