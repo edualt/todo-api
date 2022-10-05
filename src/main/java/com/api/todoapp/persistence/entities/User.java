7aa6930262719669728ff9a7fc034829d027f045
edualt <eduardosebastianhdz503@gmail.com>
2022-10-04 19:05:38 -0500

    feat(user): add new entity

---
+++ src/main/java/com/api/todoapp/persistence/entities/User.java
@@ -0,0 +1,21 @@
+package com.api.todoapp.persistence.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Getter @Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+}
